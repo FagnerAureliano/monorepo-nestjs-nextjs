56270adc3983a2b963223b300f94a5aa46012961
fagneraureliano <fagner2aureliano@gmail.com>
2023-01-27 11:27:50 -0300

    feat(server):  impl migration clients

---
+++ apps/server/prisma/migrations/20230126210355_clients/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_email_key" ON "clients"("email");

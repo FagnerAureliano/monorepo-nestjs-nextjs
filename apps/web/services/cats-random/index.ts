27086632884f30ee3c2ea61448cd4a9c4b7fd4b0
fagneraureliano <fagner2aureliano@gmail.com>
2023-01-27 11:27:08 -0300

    feat(server):  add endpoint randmUsers (extr link)

---
+++ apps/web/services/cats-random/index.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+class CatsService {
+  async getRandomCats() {
+    try {
+      const { data } = await axios.get('https://aws.random.cat/meow');
+      return data.file;
+    } catch (error) {
+      return Error(error.message);
+    }
+  }
+}
+export default new CatsService();

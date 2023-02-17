31dadbfc3d3744ca0d24b2eaa734f2cf55f7c2ae
fagneraureliano <fagner@FAGNER-NOTE>
2023-02-17 18:55:06 -0300

    feat:(web): finish page status cat

--- apps/web/pages/httpcat/index.tsx
+++ apps/web/pages/httpcat/index.tsx
@@ -1,11 +1,18 @@
+import { Loading } from '../../components/loading';
+import { useState } from 'react';
 import { Input } from '../../components/input';
 import { Layout } from '../../components/layout';
 import HttpCatsService from '../../services/http-cat';
 
 export default function HttpCat() {
+  const [catImage, setCatImage] = useState<string | null>();
+  const [loading, setLoading] = useState(false);
+
   async function handleInputChange(code) {
-    const data = await HttpCatsService.findStatusCatByCode(code);
-    console.log(data);
+    setLoading(true);
+    const { data } = await HttpCatsService.findStatusCatByCode(code);
+    setCatImage(data);
+    setLoading(false);
   }
   return (
     <>
@@ -14,6 +21,38 @@ export default function HttpCat() {
           <div className="w-60   ">
             <Input handleInput={handleInputChange} isButonSubmit={true} />
           </div>
+          {catImage ? (
+            <div>
+              {loading ? (
+                <div className="h-52">
+                  <Loading />
+                </div>
+              ) : (
+                <div className="flex justify-center m-2">
+                  <picture>
+                    <img
+                      src={catImage}
+                      className="flex object-fill rounded-lg"
+                      alt="image of cat"
+                    />
+                  </picture>
+                </div>
+              )}
+            </div>
+          ) : (
+            <>
+              <div>
+                <span className='p-2 font-bold'>Digite um número de Status HTTP</span>
+                <div className='p-2 text-base'>
+                  <p>Respostas de informação (100-199) </p>
+                  <p>Respostas de sucesso (200-299),</p>
+                  <p>Redirecionamentos (300-399)</p>
+                  <p>Erros do cliente (400-499)</p>
+                  <p> Erros do servidor (500-599).</p>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </Layout>
     </>

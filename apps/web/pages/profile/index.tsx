731772102a42b12cfadbde22d6c659cbbce9137f
FagnerAureliano <fagner2aureliano@gmail.com>
2023-03-27 15:06:15 -0300

    feat:(web) continue impl profile page

--- apps/web/pages/profile/index.tsx
+++ apps/web/pages/profile/index.tsx
@@ -1,63 +1,81 @@
-import { UserIcon } from '@heroicons/react/24/solid';
+import { PencilIcon, UserIcon } from '@heroicons/react/24/solid';
 import { Formik, Form, Field } from 'formik';
 import { useRouter } from 'next/router';
 import { userService } from '../../services/user-service';
 import { ToastContainer, toast } from 'react-toastify';
 import { useState } from 'react';
 import { Layout } from '../../components/layout';
+import { TrashIcon } from '@heroicons/react/24/outline';
 
 export default function Profile() {
   const [avatarUrl, setAvatarUrl] = useState('https://robohash.org/1234');
+  const [isDisabled, setIsDisabled] = useState(true);
   const { push } = useRouter();
-  interface ISingUp {
+  interface IUpdate {
     name: string;
     email: string;
+    old_password: string;
     password: string;
   }
 
   const initialValues = {
     name: '',
     email: '',
+    old_password: '',
     password: '',
     passwordConfirm: '',
   };
 
-  async function handleSignUp({ name, email, password }: ISingUp) {
-    if (password != initialValues.passwordConfirm) {
-      return toast.error('Erro ao cadastrar usuário. Password inválido.');
-    }
-    try {
-      const { status } = await userService.create({ name, email, password });
-      if (status === 201) {
-        toast.success('Cadastrado com sucesso !', {
-          autoClose: 1000,
-          onClose: () => push('/login'),
-        });
-      }
-    } catch (error) {
-      toast.error('Erro ao cadastrar usuário. Verifique os campos preenxidos.');
-    }
+  async function handleSignUp({
+    name,
+    email,
+    old_password,
+    password,
+  }: IUpdate) {
+    console.log(
+      name,
+      email,
+      old_password,
+      password,
+      initialValues.passwordConfirm
+    );
+
+    // if (password != initialValues.passwordConfirm) {
+    //   return toast.error('Erro ao cadastrar usuário. Password inválido.');
+    // }
+    // try {
+    //   const { status } = await userService.create({ name, email, password });
+    //   if (status === 201) {
+    //     toast.success('Cadastrado com sucesso !', {
+    //       autoClose: 1000,
+    //       onClose: () => push('/login'),
+    //     });
+    //   }
+    // } catch (error) {
+    //   toast.error('Erro ao cadastrar usuário. Verifique os campos preenxidos.');
+    // }
+  }
+
+  async function handleEditPassword(data: Event) {
+    setIsDisabled(!isDisabled);
+    console.log(data.target);
   }
 
   const generateAvatar = async () => {
     const text = Math.floor(Math.random() * 100);
-
     const response = await fetch(`https://robohash.org/${text}`);
-
     setAvatarUrl(response.url);
   };
   return (
     <>
       <Layout title="Profile">
         <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-2">
+          <div className="max-w-md w-full text-center">
             <ToastContainer autoClose={2000} />
-            <div>
+            <div className="space-y-4">
               <picture>
                 <img
-                  width={350}
-                  height={350}
-                  className="mx-auto  w-auto"
+                  className="mx-auto w-auto"
                   src={avatarUrl}
                   alt="Workflow"
                 />
@@ -70,7 +88,11 @@ export default function Profile() {
                 Refresh
               </button>
             </div>
-            <Formik initialValues={initialValues} onSubmit={handleSignUp}>
+            <Formik
+              initialValues={initialValues}
+              onReset={() => console.log('reset')}
+              onSubmit={handleSignUp}
+            >
               <Form className="mt-8 space-y-6" action="#" method="POST">
                 <input type="hidden" name="remember" defaultValue="true" />
                 <div className="rounded-md space-y-2">
@@ -98,38 +120,80 @@ export default function Profile() {
                       placeholder="E-mail"
                     />
                   </div>
-                  <div>
-                    <label htmlFor="password" className="sr-only" />
+                  <div className="flex gap-2">
+                    <label htmlFor="old_password" className="sr-only" />
                     <Field
-                      id="password"
-                      name="password"
+                      id="old_password"
+                      name="old_password"
                       type="password"
                       required
-                      className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                      placeholder="Senha"
+                      disabled={isDisabled}
+                      className="appearance-none relative block w-3/4 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                      placeholder="Senha Atual"
                     />
+
+                    <button
+                      type="button"
+                      onClick={() => handleEditPassword(event)}
+                      className="group relative w-1/4 flex justify-center py-2 px-4 border border-gray-400 text-sm font-medium rounded-md text-gray-100    bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                    >
+                      <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                        <PencilIcon
+                          className="h-5 w-5 text-gray-200 group-hover:text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                      Senha
+                    </button>
                   </div>
-                  <div>
-                    <label htmlFor="passwordConfirm" className="sr-only" />
-                    <Field
-                      id="passwordConfirm"
-                      name="passwordConfirm"
-                      type="password"
-                      required
-                      className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                      placeholder="Confirmação de senha"
-                    />
-                  </div>
+                  {!isDisabled && (
+                    <>
+                      <div>
+                        <label htmlFor="password" className="sr-only" />
+                        <Field
+                          id="password"
+                          name="password"
+                          type="password"
+                          required
+                          className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          placeholder="Nova senha"
+                        />
+                      </div>
+                      <div>
+                        <label htmlFor="passwordConfirm" className="sr-only" />
+                        <Field
+                          id="passwordConfirm"
+                          name="passwordConfirm"
+                          type="password"
+                          required
+                          className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          placeholder="Confirmar nova senha"
+                        />
+                      </div>
+                    </>
+                  )}
                 </div>
 
-                <div>
+                <div className="flex gap-2">
                   <button
                     type="submit"
-                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="group relative w-2/4 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-gray-500 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                  >
+                    <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                      <TrashIcon
+                        className="h-5 w-5 text-gray-500 group-hover:text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    Limpar
+                  </button>
+                  <button
+                    type="submit"
+                    className="group relative w-2/4 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                   >
                     <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                       <UserIcon
-                        className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                        className="h-5 w-5 text-gray-500 group-hover:text-gray-400"
                         aria-hidden="true"
                       />
                     </span>

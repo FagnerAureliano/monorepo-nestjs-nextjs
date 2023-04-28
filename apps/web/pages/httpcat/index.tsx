e58baa863c9eb12a2db1fa8a3c4796cca80c898f
fagneraureliano <fagner2aureliano@gmail.com>
2023-04-28 09:17:00 -0300

    feat(web): improve text about status cat page

--- apps/web/pages/httpcat/index.tsx
+++ apps/web/pages/httpcat/index.tsx
@@ -5,7 +5,7 @@ import { Layout } from '../../components/layout';
 import { httpStatusService } from '../../services/http-cat';
 import { toast, ToastContainer } from 'react-toastify';
 
-export default function HttpCat() {
+export default function HttpCat(props) {
   const [catImage, setCatImage] = useState<string | null>();
   const [loading, setLoading] = useState(false);
 
@@ -23,7 +23,7 @@ export default function HttpCat() {
     <>
       <Layout title="Http Status Cat">
         <ToastContainer autoClose={2000} />
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col mt-8 items-center justify-center">
           <div className="w-60">
             <Input handleInput={handleInputChange} isButonSubmit={true} />
           </div>
@@ -47,17 +47,42 @@ export default function HttpCat() {
             </div>
           ) : (
             <>
-              <div className="pt-4">
-                <span className="p-2 font-bold">
-                  Digite um número de Status HTTP
-                </span>
-                <div className="p-2 text-base gap-3">
-                  <p>Respostas de informação (100-199) </p>
-                  <p>Respostas de sucesso (200-299),</p>
-                  <p>Redirecionamentos (300-399)</p>
-                  <p>Erros do cliente (400-499)</p>
-                  <p> Erros do servidor (500-599).</p>
-                </div>
+              <div className="mt-8">
+                <p className="text-lg font-medium mb-2">
+                  Respostas de informação (100-199)
+                </p>
+                <p className="text-gray-500 mb-2">
+                  As respostas de informação indicam que o servidor recebeu a
+                  requisição do cliente e está processando-a.
+                </p>
+                <p className="text-lg font-medium mb-2">
+                  Respostas de sucesso (200-299)
+                </p>
+                <p className="text-gray-500 mb-2">
+                  As respostas de sucesso indicam que a requisição do cliente
+                  foi bem sucedida e o servidor retornou uma resposta.
+                </p>
+                <p className="text-lg font-medium mb-2">
+                  Redirecionamentos (300-399)
+                </p>
+                <p className="text-gray-500 mb-2">
+                  As respostas de redirecionamento indicam que o cliente precisa
+                  tomar alguma ação adicional para completar a requisição.
+                </p>
+                <p className="text-lg font-medium mb-2">
+                  Erros do cliente (400-499)
+                </p>
+                <p className="text-gray-500 mb-2">
+                  As respostas de erro do cliente indicam que a requisição do
+                  cliente contém erros.
+                </p>
+                <p className="text-lg font-medium mb-2">
+                  Erros do servidor (500-599)
+                </p>
+                <p className="text-gray-500 mb-2">
+                  As respostas de erro do servidor indicam que o servidor
+                  encontrou um erro ao processar a requisição do cliente.
+                </p>
               </div>
             </>
           )}
